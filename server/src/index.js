6aa5b65f434355d6ed02ce9dd49e7db3f0c7907d
Yard <wengyd@huoqiuapp.com>
2018-02-05 18:15:33 +0800

    v

---
+++ server/src/index.js
@@ -0,0 +1,4 @@
+require('./request')({
+  mobile: '15700084187',
+  url: 'https://h5.ele.me/hongbao/#hardware_id=&is_lucky_group=True&lucky_number=9&track_id=&platform=0&sn=29e6af74efac5872&theme_id=2121&device_id='
+})
